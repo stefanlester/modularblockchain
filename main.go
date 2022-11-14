096a19b201d5d735072e9d1cdda9ce03148e015b
STEFAN AMOAH <stefanamoah001@gmail.com>
2022-11-14 23:19:24 +0100

    refactor: new transport layer

--- main.go
+++ main.go
@@ -1,17 +1,27 @@
 package main
 
 import (
-	"fmt"
-	"github.com/stefanlester/Modular-Blockchain/network"
+	"time"
+
+	"github.com/stefanlester/modularblockchain/network"
 )
 
 func main() {
 	trLocal := network.NewLocalTransport("LOCAL")
+	trRemote := network.NewLocalTransport("REMOTE")
+
+	trLocal.Connect(trRemote)
+	trRemote.Connect(trLocal)
+
+	go func() {
+		trRemote.SendMessage(trLocal.Addr(), []byte("hello world"))
+		time.Sleep(1 * time.Second)
+	}()
 
 	opts := network.ServerOpts{
 		Transports: []network.Transport{trLocal},
 	}
 
 	srv := network.NewServer(opts)
+	srv.Start()
 }
-
--- network/server.go
+++ network/server.go
@@ -1,6 +1,9 @@
 package network
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 type ServerOpts struct {
 	Transports []Transport
@@ -8,25 +11,44 @@ type ServerOpts struct {
 
 type Server struct {
 	ServerOpts
-	rpcCh chan RPC
+	rpcCh  chan RPC
+	quitCh chan struct{}
 }
 
 func NewServer(opts ServerOpts) *Server {
 	return &Server{
 		ServerOpts: opts,
 		rpcCh:      make(chan RPC),
+		quitCh:     make(chan struct{}, 1),
 	}
 }
 
 func (s *Server) Start() {
 	s.initTransport()
+	ticker := time.NewTicker(5 * time.Second)
+
+free:
+	for {
+		select {
+		case rpc := <-s.rpcCh:
+			fmt.Printf("%+v", rpc)
+		case <-s.quitCh:
+			break free
+		case <-ticker.C:
+			fmt.Println("do stuff every X seconds")
+		default:
+			continue 
+		}
+	}
+
+	fmt.Println("server quit")
 }
 
 func (s *Server) initTransport() {
 	for _, tr := range s.Transports {
 		go func(tr Transport) {
 			for rpc := range tr.Consume() {
-				fmt.Println(rpc)
+				s.rpcCh <- rpc
 			}
 		}(tr)
 	}
