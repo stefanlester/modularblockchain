3147c824906696057e3cb84b17bfc4a0770862f6
STEFAN AMOAH <stefanamoah001@gmail.com>
2022-11-22 12:44:18 +0100

    feat: encoding and decoding binaries

--- core/block.go
+++ core/block.go
@@ -1,6 +1,9 @@
 package core
 
 import (
+	"encoding/binary"
+	"io"
+
 	"github.com/stefanlester/modularblockchain/types"
 )
 
@@ -10,6 +13,46 @@ type Header struct {
 	Timestamp uint64
 	Height    uint32
 	Nonce     uint64
+}
+
+func (h *Header) EncodeBinary(w io.Writer) error {
+	if err := binary.Write(w, binary.LittleEndian, &h.Version); err != nil {
+		return err
+	}
+
+	if err := binary.Write(w, binary.LittleEndian, &h.PrevBlock); err != nil {
+		return err
+	}
+
+	if err := binary.Write(w, binary.LittleEndian, &h.Timestamp); err != nil {
+		return err
+	}
+
+	if err := binary.Write(w, binary.LittleEndian, &h.Height); err != nil {
+		return err
+	}
+
+	return binary.Write(w, binary.LittleEndian, &h.Nonce)
+}
+
+func (h *Header) DecodeBinary(r io.Reader) error {
+	if err := binary.Read(r, binary.LittleEndian, &h.Version); err != nil {
+		return err
+	}
+
+	if err := binary.Read(r, binary.LittleEndian, &h.PrevBlock); err != nil {
+		return err
+	}
+
+	if err := binary.Read(r, binary.LittleEndian, &h.Timestamp); err != nil {
+		return err
+	}
+
+	if err := binary.Read(r, binary.LittleEndian, &h.Height); err != nil {
+		return err
+	}
+
+	return binary.Read(r, binary.LittleEndian, &h.Nonce)
 }
 
 type Block struct {
--- types/hash.go
+++ types/hash.go
@@ -1,3 +1,19 @@
 package types
 
+import "fmt"
+
 type Hash [32]uint8
+
+func HashFromBytes(b []byte) Hash {
+	if len(b) != 32 {
+		msg := fmt.Sprintf("given bytes with length %d should be 32", len(b))
+		panic(msg)
+	}
+
+	var value [32]uint8
+	for i := 0; i < 32; i++ {
+		value[i] = b[i]
+	}
+
+	return Hash(value)
+}
