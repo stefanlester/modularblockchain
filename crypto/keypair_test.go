c089f50ea5689e8aaf4fcea70db8b2ebcfd1fbdc
STEFAN AMOAH <stefanamoah001@gmail.com>
2022-11-23 19:23:29 +0100

    test: key pair generation

--- crypto/keypair_test.go
+++ crypto/keypair_test.go
@@ -9,12 +9,16 @@ import (
 
 func TestGeneratePrivateKey(t *testing.T) {
 	privKey := GeneratePrivateKey()
-	// pubKey := privKey.PublicKey()
+	pubKey := privKey.PublicKey()
 	// address := pubKey.Address()
 
 	msg := []byte("Hello, world!")
 	sig, err := privKey.Sign(msg)
 	assert.Nil(t, err)
+
+	sig.Verify(pubKey, msg)
+
+	assert.True(t, sig.Verify(pubKey, msg))
 
 	fmt.Println(sig)
 }
