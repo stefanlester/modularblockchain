876e82348a939e2b5012db8dfdb95a5861bfcac2
STEFAN AMOAH <stefanamoah001@gmail.com>
2022-11-28 16:48:46 +0100

    Hash will hash the whole bytes of the TX no exception

---
+++ core/hasher.go
@@ -0,0 +1,35 @@
+package core
+
+import (
+	"bytes"
+	"crypto/sha256"
+	"encoding/binary"
+
+	"github.com/stefanlester/modularblockchain/types"
+)
+
+type Hasher[T any] interface {
+	Hash(T) types.Hash
+}
+
+type BlockHasher struct{}
+
+func (BlockHasher) Hash(b *Header) types.Hash {
+	h := sha256.Sum256(b.Bytes())
+	return types.Hash(h)
+}
+
+type TxHasher struct{}
+
+// Hash will hash the whole bytes of the TX no exception.
+func (TxHasher) Hash(tx *Transaction) types.Hash {
+	buf := new(bytes.Buffer)
+
+	binary.Write(buf, binary.LittleEndian, tx.Data)
+	binary.Write(buf, binary.LittleEndian, tx.To)
+	binary.Write(buf, binary.LittleEndian, tx.Value)
+	binary.Write(buf, binary.LittleEndian, tx.From)
+	binary.Write(buf, binary.LittleEndian, tx.Nonce)
+
+	return types.Hash(sha256.Sum256(buf.Bytes()))
+}
