755966ab6cfcb8d511ba13978b8ae23c8578c644
STEFAN AMOAH <stefanamoah001@gmail.com>
2022-11-29 12:30:02 +0100

    refactor: block and calculating data hash

--- core/block.go
+++ core/block.go
@@ -3,101 +3,136 @@ package core
 import (
 	"bytes"
 	"crypto/sha256"
-	"encoding/binary"
-	"io"
+	"encoding/gob"
+	"fmt"
+	"time"
 
+	"github.com/stefanlester/modularblockchain/crypto"
 	"github.com/stefanlester/modularblockchain/types"
 )
 
 type Header struct {
-	Version   uint32
-	PrevBlock types.Hash
-	Timestamp int64
-	Height    uint32
-	Nonce     uint64
+	Version       uint32
+	DataHash      types.Hash
+	PrevBlockHash types.Hash
+	Height        uint32
+	Timestamp     int64
 }
 
-func (h *Header) EncodeBinary(w io.Writer) error {
-	if err := binary.Write(w, binary.LittleEndian, &h.Version); err != nil {
-		return err
-	}
+func (h *Header) Bytes() []byte {
+	buf := &bytes.Buffer{}
+	enc := gob.NewEncoder(buf)
+	enc.Encode(h)
 
-	if err := binary.Write(w, binary.LittleEndian, &h.PrevBlock); err != nil {
-		return err
-	}
-
-	if err := binary.Write(w, binary.LittleEndian, &h.Timestamp); err != nil {
-		return err
-	}
-
-	if err := binary.Write(w, binary.LittleEndian, &h.Height); err != nil {
-		return err
-	}
-
-	return binary.Write(w, binary.LittleEndian, &h.Nonce)
-}
-
-func (h *Header) DecodeBinary(r io.Reader) error {
-	if err := binary.Read(r, binary.LittleEndian, &h.Version); err != nil {
-		return err
-	}
-
-	if err := binary.Read(r, binary.LittleEndian, &h.PrevBlock); err != nil {
-		return err
-	}
-
-	if err := binary.Read(r, binary.LittleEndian, &h.Timestamp); err != nil {
-		return err
-	}
-
-	if err := binary.Read(r, binary.LittleEndian, &h.Height); err != nil {
-		return err
-	}
-
-	return binary.Read(r, binary.LittleEndian, &h.Nonce)
+	return buf.Bytes()
 }
 
 type Block struct {
-	Header
-	Transactions []Transaction
+	*Header
+
+	Transactions []*Transaction
+	Validator    crypto.PublicKey
+	Signature    *crypto.Signature
 
 	// Cached version of the header hash
 	hash types.Hash
 }
 
-func (b *Block) Hash() types.Hash {
-	buf := &bytes.Buffer{}
-	b.Header.EncodeBinary(buf)
+func NewBlock(h *Header, txx []*Transaction) (*Block, error) {
+	return &Block{
+		Header:       h,
+		Transactions: txx,
+	}, nil
+}
 
-	b.hash = types.Hash(sha256.Sum256(buf.Bytes()))
+func NewBlockFromPrevHeader(prevHeader *Header, txx []*Transaction) (*Block, error) {
+	dataHash, err := CalculateDataHash(txx)
+	if err != nil {
+		return nil, err
+	}
+
+	header := &Header{
+		Version:       1,
+		Height:        prevHeader.Height + 1,
+		DataHash:      dataHash,
+		PrevBlockHash: BlockHasher{}.Hash(prevHeader),
+		Timestamp:     time.Now().UnixNano(),
+	}
+
+	return NewBlock(header, txx)
+}
+
+func (b *Block) AddTransaction(tx *Transaction) {
+	b.Transactions = append(b.Transactions, tx)
+	hash, _ := CalculateDataHash(b.Transactions)
+	b.DataHash = hash
+}
+
+func (b *Block) Sign(privKey crypto.PrivateKey) error {
+	sig, err := privKey.Sign(b.Header.Bytes())
+	if err != nil {
+		return err
+	}
+
+	b.Validator = privKey.PublicKey()
+	b.Signature = sig
+
+	return nil
+}
+
+func (b *Block) Verify() error {
+	if b.Signature == nil {
+		return fmt.Errorf("block has no signature")
+	}
+
+	if !b.Signature.Verify(b.Validator, b.Header.Bytes()) {
+		return fmt.Errorf("block has invalid signature")
+	}
+
+	for _, tx := range b.Transactions {
+		if err := tx.Verify(); err != nil {
+			return err
+		}
+	}
+
+	dataHash, err := CalculateDataHash(b.Transactions)
+	if err != nil {
+		return err
+	}
+
+	if dataHash != b.DataHash {
+		return fmt.Errorf("block (%s) has an invalid data hash", b.Hash(BlockHasher{}))
+	}
+
+	return nil
+}
+
+func (b *Block) Decode(dec Decoder[*Block]) error {
+	return dec.Decode(b)
+}
+
+func (b *Block) Encode(enc Encoder[*Block]) error {
+	return enc.Encode(b)
+}
+
+func (b *Block) Hash(hasher Hasher[*Header]) types.Hash {
+	if b.hash.IsZero() {
+		b.hash = hasher.Hash(b.Header)
+	}
 
 	return b.hash
 }
 
-func (b *Block) EncodeBinary(w io.Writer) error {
-	if err := b.Header.EncodeBinary(w); err != nil {
-		return err
-	}
+func CalculateDataHash(txx []*Transaction) (hash types.Hash, err error) {
+	buf := &bytes.Buffer{}
 
-	for _, tx := range b.Transactions {
-		if err := tx.EncodeBinary(w); err != nil {
-			return err
+	for _, tx := range txx {
+		if err = tx.Encode(NewGobTxEncoder(buf)); err != nil {
+			return
 		}
 	}
 
-	return nil
-}
+	hash = sha256.Sum256(buf.Bytes())
 
-func (b *Block) DecodeBinary(r io.Reader) error {
-	if err := b.Header.DecodeBinary(r); err != nil {
-		return err
-	}
-
-	for _, tx := range b.Transactions {
-		if err := tx.DecodeBinary(r); err != nil {
-			return err
-		}
-	}
-
-	return nil
+	return
 }
