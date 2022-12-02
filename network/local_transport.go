e6da4063f08df05cd2b1b88cc9773e79e7265826
STEFAN AMOAH <stefanamoah001@gmail.com>
2022-12-02 01:49:34 +0100

    feat: get block message

--- network/local_transport.go
+++ network/local_transport.go
@@ -1,22 +1,24 @@
 package network
 
 import (
+	"bytes"
 	"fmt"
+	"net"
 	"sync"
 )
 
 type LocalTransport struct {
-	addr      NetAddr
+	addr      net.Addr
 	consumeCh chan RPC
 	lock      sync.RWMutex
-	peers     map[NetAddr]*LocalTransport
+	peers     map[net.Addr]*LocalTransport
 }
 
-func NewLocalTransport(addr NetAddr) Transport {
+func NewLocalTransport(addr net.Addr) *LocalTransport {
 	return &LocalTransport{
 		addr:      addr,
 		consumeCh: make(chan RPC, 1024),
-		peers:     make(map[NetAddr]*LocalTransport),
+		peers:     make(map[net.Addr]*LocalTransport),
 	}
 }
 
@@ -25,32 +27,45 @@ func (t *LocalTransport) Consume() <-chan RPC {
 }
 
 func (t *LocalTransport) Connect(tr Transport) error {
+	trans := tr.(*LocalTransport)
 	t.lock.Lock()
 	defer t.lock.Unlock()
 
-	t.peers[tr.Addr()] = tr.(*LocalTransport)
+	t.peers[tr.Addr()] = trans
 
 	return nil
 }
 
-func (t *LocalTransport) SendMessage(to NetAddr, payload []byte) error {
+func (t *LocalTransport) SendMessage(to net.Addr, payload []byte) error {
 	t.lock.RLock()
 	defer t.lock.RUnlock()
 
-	peer, ok := t.peers[to]
+	if t.addr == to {
+		return nil
+	}
 
+	peer, ok := t.peers[to]
 	if !ok {
-		return fmt.Errorf("%s: could not send message to %s", t.addr, to)
+		return fmt.Errorf("%s: could not send message to unknown peer %s", t.addr, to)
 	}
 
 	peer.consumeCh <- RPC{
 		From:    t.addr,
-		Payload: payload,
+		Payload: bytes.NewReader(payload),
 	}
 
 	return nil
 }
 
-func (t *LocalTransport) Addr() NetAddr {
+func (t *LocalTransport) Broadcast(payload []byte) error {
+	for _, peer := range t.peers {
+		if err := t.SendMessage(peer.Addr(), payload); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (t *LocalTransport) Addr() net.Addr {
 	return t.addr
 }
