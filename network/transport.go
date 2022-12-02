9e0925417e122360e1cd0627f47c41991da7ed4e
STEFAN AMOAH <stefanamoah001@gmail.com>
2022-12-02 22:03:23 +0100

    fix minor bug in transport; remove rpc

--- network/transport.go
+++ network/transport.go
@@ -1,15 +1,13 @@
 package network
 
-type NetAddr string
+import "net"
 
-type RPC struct {
-	From    NetAddr
-	Payload []byte
-}
+type NetAddr string
 
 type Transport interface {
 	Consume() <-chan RPC
 	Connect(Transport) error
-	SendMessage(NetAddr, []byte) error
-	Addr() NetAddr
+	SendMessage(net.Addr, []byte) error
+	Broadcast([]byte) error
+	Addr() net.Addr
 }
