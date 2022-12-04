4e03d9a8750b4ed69bdf46633094873b68dde904
STEFAN AMOAH <stefanamoah001@gmail.com>
2022-12-04 17:24:39 +0100

    test: tx pool plus refactoring in main

--- main.go
+++ main.go
@@ -1,27 +1,165 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
+	"log"
+	"net/http"
 	"time"
 
+	"github.com/stefanlester/modularblockchain/core"
+	"github.com/stefanlester/modularblockchain/crypto"
 	"github.com/stefanlester/modularblockchain/network"
+	"github.com/stefanlester/modularblockchain/types"
+	"github.com/stefanlester/modularblockchain/util"
 )
 
 func main() {
-	trLocal := network.NewLocalTransport("LOCAL")
-	trRemote := network.NewLocalTransport("REMOTE")
+	validatorPrivKey := crypto.GeneratePrivateKey()
+	localNode := makeServer("LOCAL_NODE", &validatorPrivKey, ":3000", []string{":4000"}, ":9000")
+	go localNode.Start()
 
-	trLocal.Connect(trRemote)
-	trRemote.Connect(trLocal)
+	remoteNode := makeServer("REMOTE_NODE", nil, ":4000", []string{":5000"}, "")
+	go remoteNode.Start()
+
+	remoteNodeB := makeServer("REMOTE_NODE_B", nil, ":5000", nil, "")
+	go remoteNodeB.Start()
 
 	go func() {
-		trRemote.SendMessage(trLocal.Addr(), []byte("hello world"))
-		time.Sleep(1 * time.Second)
+		time.Sleep(11 * time.Second)
+
+		lateNode := makeServer("LATE_NODE", nil, ":6000", []string{":4000"}, "")
+		go lateNode.Start()
 	}()
 
-	opts := network.ServerOpts{
-		Transports: []network.Transport{trLocal},
+	time.Sleep(1 * time.Second)
+
+	// if err := sendTransaction(validatorPrivKey); err != nil {
+	// 	panic(err)
+	// }
+
+	// collectionOwnerPrivKey := crypto.GeneratePrivateKey()
+	// collectionHash := createCollectionTx(collectionOwnerPrivKey)
+
+	// txSendTicker := time.NewTicker(1 * time.Second)
+	// go func() {
+	// 	for i := 0; i < 20; i++ {
+	// 		nftMinter(collectionOwnerPrivKey, collectionHash)
+
+	// 		<-txSendTicker.C
+	// 	}
+	// }()
+
+	select {}
+}
+
+func sendTransaction(privKey crypto.PrivateKey) error {
+	toPrivKey := crypto.GeneratePrivateKey()
+
+	tx := core.NewTransaction(nil)
+	tx.To = toPrivKey.PublicKey()
+	tx.Value = 666
+
+	if err := tx.Sign(privKey); err != nil {
+		return err
 	}
 
-	srv := network.NewServer(opts)
-	srv.Start()
+	buf := &bytes.Buffer{}
+	if err := tx.Encode(core.NewGobTxEncoder(buf)); err != nil {
+		panic(err)
+	}
+
+	req, err := http.NewRequest("POST", "http://localhost:9000/tx", buf)
+	if err != nil {
+		panic(err)
+	}
+
+	client := http.Client{}
+	_, err = client.Do(req)
+
+	return err
+}
+
+func makeServer(id string, pk *crypto.PrivateKey, addr string, seedNodes []string, apiListenAddr string) *network.Server {
+	opts := network.ServerOpts{
+		APIListenAddr: apiListenAddr,
+		SeedNodes:     seedNodes,
+		ListenAddr:    addr,
+		PrivateKey:    pk,
+		ID:            id,
+	}
+
+	s, err := network.NewServer(opts)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return s
+}
+
+func createCollectionTx(privKey crypto.PrivateKey) types.Hash {
+	tx := core.NewTransaction(nil)
+	tx.TxInner = core.CollectionTx{
+		Fee:      200,
+		MetaData: []byte("chicken and egg collection!"),
+	}
+	tx.Sign(privKey)
+
+	buf := &bytes.Buffer{}
+	if err := tx.Encode(core.NewGobTxEncoder(buf)); err != nil {
+		panic(err)
+	}
+
+	req, err := http.NewRequest("POST", "http://localhost:9000/tx", buf)
+	if err != nil {
+		panic(err)
+	}
+
+	client := http.Client{}
+	_, err = client.Do(req)
+	if err != nil {
+		panic(err)
+	}
+
+	return tx.Hash(core.TxHasher{})
+}
+
+func nftMinter(privKey crypto.PrivateKey, collection types.Hash) {
+	metaData := map[string]any{
+		"power":  8,
+		"health": 100,
+		"color":  "green",
+		"rare":   "yes",
+	}
+
+	metaBuf := new(bytes.Buffer)
+	if err := json.NewEncoder(metaBuf).Encode(metaData); err != nil {
+		panic(err)
+	}
+
+	tx := core.NewTransaction(nil)
+	tx.TxInner = core.MintTx{
+		Fee:             200,
+		NFT:             util.RandomHash(),
+		MetaData:        metaBuf.Bytes(),
+		Collection:      collection,
+		CollectionOwner: privKey.PublicKey(),
+	}
+	tx.Sign(privKey)
+
+	buf := &bytes.Buffer{}
+	if err := tx.Encode(core.NewGobTxEncoder(buf)); err != nil {
+		panic(err)
+	}
+
+	req, err := http.NewRequest("POST", "http://localhost:9000/tx", buf)
+	if err != nil {
+		panic(err)
+	}
+
+	client := http.Client{}
+	_, err = client.Do(req)
+	if err != nil {
+		panic(err)
+	}
 }
